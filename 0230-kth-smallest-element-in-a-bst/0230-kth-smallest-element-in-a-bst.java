65e3474ce41585fce35374ea9e0eefe4f3afb970
박진영 <94813918+jny0@users.noreply.github.com>
2023-09-08 00:22:30 +0900

    Time: 1 ms (47.18%), Space: 43.9 MB (83.17%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -13,20 +13,29 @@
  *     }
  * }
  */
-class Solution {
-    static int a;
-    static int answer;
-    public int kthSmallest(TreeNode root, int k) {
-        a = k;
-        inorder(root);
-        return answer;
-    }
 
-    public void inorder(TreeNode now){
-        if(now == null) return;
-        inorder(now.left);
-        a--;
-        if(a==0) answer =  now.val;
-        inorder(now.right);
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        int count = 0;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            count++;
+
+            if (count == k) {
+                return current.val;
+            }
+
+            current = current.right;
+        }
+
+        return -1; // k번째 원소가 항상 존재한다고 가정할 때, 여기에 다른 처리를 추가할 수 있습니다.
     }
 }
